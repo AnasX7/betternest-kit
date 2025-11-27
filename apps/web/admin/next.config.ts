92c5cd301b3d1c002338e79fdbba24c2098c4932
Anas Salem <anassalem.aa@gmail.com>
2025-11-27 21:11:04 +0300

    update nextjs to version 16

---
+++ apps/web/admin/next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  reactCompiler: true,
+  typedRoutes: true,
+  experimental: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.microlink.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+}
+
+export default nextConfig
